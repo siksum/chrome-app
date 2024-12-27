b7c8259a995d2b4ab1e56a62ee02c6437643f154
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 17:32:16 +0900

    7.2 Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,22 @@ const toDoInput = toDoForm.querySelector("input");
 // document.querySelelctor("#todo-form input")과 동일
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
