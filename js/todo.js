d20473e29698d808562b4fd3113716514eb328b3
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 17:11:24 +0900

    7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 // document.querySelelctor("#todo-form input")과 동일
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    console.log(newTodo, toDoInput.value)
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
