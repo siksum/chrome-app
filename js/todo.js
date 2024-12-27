7a6745ee5d083855b4cf7c1600fb53bb546240f1
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 18:09:46 +0900

    7.4 Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 // document.querySelelctor("#todo-form input")과 동일
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
 const toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -37,3 +38,10 @@ function handleToDoSubmit(event){
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of ", item));
+}
