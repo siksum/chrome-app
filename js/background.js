59d1fc70840fb91ea4bedb3a611a9cf3d563ca9f
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-28 00:43:39 +0900

    add: background transition

--- js/background.js
+++ js/background.js
@@ -2,7 +2,17 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.src = `img/${chosenImage}`;
+// const bgImage = document.createElement("img");
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+const backgroundDiv = document.getElementById("background");
+
+backgroundDiv.style.backgroundImage = `url(img/${chosenImage})`;
+backgroundDiv.classList.add('fade-out');
+    
+setTimeout(() => {
+    backgroundDiv.style.backgroundImage = `url(img/${chosenImage})`;
+    backgroundDiv.classList.remove('fade-out');
+}, 500);
