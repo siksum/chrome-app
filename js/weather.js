7b4dfa7c0ce57abed26fbf76faa9f93246e7bf80
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 23:47:07 +0900

    8.1 Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,16 @@
+const API_KEY = "ea30739a9d8efec94e7ce5500f704570";
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&unitsgi=metric`
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+		const weather = document.querySelector("#weather span:first-child");
+		const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError(){
