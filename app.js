1447799a2c6b797164fa267f0b52789d61ef29e6
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 15:02:22 +0900

    Saving Username

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    const username = loginInput.value;
+
+    localStorage.setItem("username", username);
+
     // greeting.innerText = "Hello " + username;
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
