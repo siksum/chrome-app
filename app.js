4176a6057f5bbdc6c09a1d7aa26ecc4399ec06c2
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2024-12-27 14:38:17 +0900

    Events part two

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(tomato){
     tomato.preventDefault();
     console.log(tomato);		
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked!");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
